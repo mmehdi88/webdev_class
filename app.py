baba9510929325baf81666fe5e4d67475999f004
Mohsin Mehdi <mmehdi88@gmail.comt>
2018-08-31 20:04:13 -0500

    updated files, added display_results

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#Maintainer Qamber Mehdi
+# Created on August 25, 2018
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+import requests, json
 
+def search_businesses(search_term, search_location):
 
-# Automatically reload the application upon a template file change
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+    api_key = "z6Ki-01wJkXzlDyFkdLMp-uhkqdC5ZnMJmQH2QdXfpYU3Z15zh5HbVhCM24LNiQ1BVib8rwP5PLMPATJblk3tuAVjfw_rp5w37w1SiXSKR1YXp0mOmCY-ZCVvotWW3Yx"
 
-#run the application
-port = 5000
-app.run(host="0.0.0.0", port=port, debug=True)
+    url = "https://api.yelp.com/v3/businesses/search"
+
+    my_headers = {
+        "Authorization": "Bearer %s" % api_key
+    }
+
+    my_params = {
+        "term": search_term,
+        "location": search_location,
+        "limit": 3,
+    }
+
+    businesses_object = requests.get(url, headers=my_headers, params=my_params)
+
+    businesses_dict = json.loads(businesses_object.text)
+
+    businesses_list = businesses_dict['businesses']
+    #
+    list_of_businesses = []
+    for each in businesses_list:
+        list_of_businesses.append(each)
+    return list_of_businesses
