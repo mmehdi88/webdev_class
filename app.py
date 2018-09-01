f8e89e8807b3a11fde2090c2adaa9517e63affa5
Mohsin Mehdi <mmehdi88@gmail.comt>
2018-09-01 11:18:43 -0500

    added main method correction

--- app.py
+++ app.py
@@ -20,6 +20,6 @@ def submit():
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 #run the application
-if __name__: "__main__"
+if __name__== '__main__':
     port = 5000
     app.run(host="0.0.0.0", port=port, debug=True)
